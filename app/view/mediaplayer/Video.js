1880b400c50f4d9e937734fc3dc5a1b21ec68fb3
wenner <wenner.cn@gmail.com>
2012-07-27 17:24:57 +0800

    测试调用plugins VideoPlayer

--- app/view/mediaplayer/Video.js
+++ app/view/mediaplayer/Video.js
@@ -35,7 +35,21 @@
 	setVideo: function(media){
 		if (!media) return;
 		var el = 	this.down("video");
-		alert(media.get("url"));
+		/*
+		window.plugins.videoPlayer.play("http://path.to.my/video.mp4");
+		window.plugins.videoPlayer.play("file:///path/to/my/video.mp4");
+		window.plugins.videoPlayer.play("file:///android_asset/www/path/to/my/video.mp4");
+		*/
+		alert("视频地址:"+media.get("url"));
+		alert(window.plugins.videoPlayer.play);
+
+		try{
+			window.plugins.videoPlayer.play(media.get("url"));
+			return;
+		}catch(e){
+			alert(e.message);
+		}
+
 		el.setUrl(media.get("url"));
 		el.play();
 		if (el.isPlaying()){
